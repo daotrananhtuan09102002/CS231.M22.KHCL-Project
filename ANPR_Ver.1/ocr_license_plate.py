a7d2aeefea145a2c060ab4053924f7f3a94198b5
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2022-05-21 14:48:48 +0700

    complete project

--- ANPR_Ver.1/ocr_license_plate.py
+++ ANPR_Ver.1/ocr_license_plate.py
@@ -6,6 +6,7 @@ import imutils
 import cv2
 from character_segmentation import character_segmentation
 import numpy as np
+import time
 
 
 def parse_args():
@@ -26,9 +27,10 @@ def main(args):
     seg_anpr = character_segmentation(n_clusters=3, debug=args["debug"] > 0)
 
     # grab all image paths in the input directory
-    imagePaths = sorted(list(paths.list_images(args["input"])))[0:500]
+    imagePaths = sorted(list(paths.list_images(args["input"])))
     # loop over all image paths in the input directory
     count = 0
+    start = time.time()
     for _, imagePath in enumerate(imagePaths, start=1):
         # load the input image from disk and resize it
         image = cv2.imread(imagePath)
@@ -39,7 +41,7 @@ def main(args):
         print(f'Processing image: {_}')
         if len(lpCnt) == 0:
             print("Can't detect license plate")
-            cv2.imshow("Output", image)
+            cv2.imshow(f"Output{_}", image)
             cv2.waitKey(0)
             continue
         flag = False
@@ -51,14 +53,19 @@ def main(args):
                 box = cv2.boxPoints(rect)
                 box = np.int0(box)
                 cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+                x, y, w, h = cv2.boundingRect(lpCnt[i])
+                cv2.putText(image, '{}'.format(char.replace('\n', '-')),
+                             (x - 15, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
                 cv2.imshow(f"Output_{_}", image)
                 print('Characters detected:\n' + char)
                 flag = True
+
         cv2.waitKey(0)
         if flag is False:
             print('No characters detected')
         cv2.destroyAllWindows()
 
+    print(f'Total time: {time.time() - start}')
     print('Detected {} license plates'.format(count))
     print('Number of images: {}'.format(len(imagePaths)))
     print('Percentage of detected license plates: {:.2f}'.format(count / len(imagePaths)))
